b95f1728219d25e4814b46fad9c566e2e4cbcd1a
Jovan Ivanovic <jovan.ivanovic3@gmail.com>
2016-11-18 23:06:37 +0100

    Instance status check

--- awsCaller.js
+++ awsCaller.js
@@ -1,12 +1,30 @@
 'use strict';
 
-var awsSdk = require('aws-sdk');
+var AWS = require('aws-sdk');
+var ec2 = new AWS.EC2({apiVersion: '2016-09-15'});
+
+AWS.config.update({region:'us-west-2'});
 
 var AwsCaller = {
   CheckServiceStatus: function (service) {
-   if (service === "elastic beanstalk") {
-     return "blablabla";
-   }
+    if (service === "ec2") {
+      var params = {
+        IncludeAllInstances: true
+      };
+
+      ec2.waitFor('instanceStatusOk', params, function(err, data) {
+        if (err) {
+          console.log(err, err.stack);
+        } else {
+          return data.InstanceStatuses.map(function(instance) {
+            return {
+              id: instance.InstanceId,
+              state: instance.InstanceState.Name
+            };
+          });
+        }
+      });
+    }
   }
 };
 
