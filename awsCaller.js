c6b7498d9463b353faf12828ba33d32331bc7c96
nemus993 <ndecoy@gmail.com>
2016-11-19 06:08:23 +0100

    Add CheckBillingCost for your aws

--- awsCaller.js
+++ awsCaller.js
@@ -1,20 +1,27 @@
 'use strict';
 
+var billing = require('aws-billing');
 var AWS = require('aws-sdk');
 var ec2 = new AWS.EC2({apiVersion: '2016-09-15'});
 
-AWS.config.update({region:'us-west-2'});
+AWS.config.update({region:'us-east-1'});
 
 var AwsCaller = {
   CheckServiceStatus: function (service, callback) {
      if (service === "ec2") {
        var request = ec2.describeInstances({}, function(err, data) {
          if(err) console.log(err, err.stack); // an error has happened on AWS
-         console.log("finished before callback");
 
          callback(data);
        })
     }
+  },
+  CheckBillingStatus: function (callback) {
+    billing( function (err, cost) {
+      if (err) console.log(err, err.stack);
+
+      callback(cost);
+    });
   }
 };
 
